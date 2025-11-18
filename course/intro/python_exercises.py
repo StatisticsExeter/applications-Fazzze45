efe5543a3c5881bb76e91036ee963f3bfb087039
Fazzze45 <fufusyed.234@gmail.com>
2025-11-18 04:10:37 +0000

    Update python_exercises.py
    Add missing python_exercises functions

--- course/intro/python_exercises.py
+++ course/intro/python_exercises.py
@@ -2,7 +2,6 @@ def sum_list(nums):
     """Return the sum of a list of numbers."""
     return sum(nums)
 
-
 def multiply_list(nums):
     """Return the product of a list of numbers."""
     result = 1
@@ -10,12 +9,14 @@ def multiply_list(nums):
         result *= n
     return result
 
-
 def reverse_string(s):
     """Return the reverse of a string."""
     return s[::-1]
 
-
 def max_value(nums):
     """Return the maximum value in a list of numbers."""
     return max(nums)
+
+def filter_even(nums):
+    """Return only even numbers from the list."""
+    return [n for n in nums if n % 2 == 0]
